05ab7eddc2f01d83fa1b4dd97cf89a386491f547
dbcr1 <eldar.kuzhekov@yandex.ru>
2022-08-12 18:46:54 +0400

    /

---
+++ test_serial.py
@@ -0,0 +1,21 @@
+import serial
+from time import sleep
+
+robot = serial.Serial('COM3', 115200)
+START_BYTE = 0xFF
+END_BYTE = 0xFE
+
+buffer = [' ' for i in range(6)]
+
+buffer[0] = START_BYTE
+buffer[1] = 1
+buffer[2] = 0
+buffer[3] = 1
+buffer[4] = 0
+buffer[5] = END_BYTE
+
+print (buffer)
+
+robot.write(buffer)
+
+robot.close()
