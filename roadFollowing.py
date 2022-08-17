4c6ff9b13cec6a9b64290ea07c2e92e0067696ff
dbcr1 <eldar.kuzhekov@yandex.ru>
2022-08-17 19:59:23 +0400

    .

--- roadFollowing.py
+++ roadFollowing.py
@@ -1,12 +1,9 @@
 import cv2 as cv
-from flask import Flask, render_template, Response, request
 import time
-import argparse
 import serial
 import numpy as np
 
 camera = cv.VideoCapture(0)
-app = Flask(__name__)
 
 robot = serial.Serial('/dev/ttyACM0', 115200)
 START_BYTE = 0xFF
@@ -15,14 +12,7 @@ END_BYTE = 0xFE
 buffer = [' ' for i in range(6)]
 buffer[0] = START_BYTE
 buffer[5] = END_BYTE
-buffer[1] = 1
-buffer[2] = 0
-buffer[3] = 1
-buffer[4] = 0
-time.sleep(1)
-robot.write(buffer)
 
-ek = 0
 speed = 110
 
 size = [240, 320]
@@ -37,12 +27,15 @@ dst = np.float32([[0, size[0]],
                     [0, 0]])
 
 src_draw = np.array(src, dtype=np.int32)
+
+
 def clamp(x, min, max):
     if x < min:
         return min
     if x > max:
         return max
     return x
+
 
 def getFramesGenerator():
     """ Генератор фреймов для вывода в веб-страницу, тут же можно поиграть с openCV"""
@@ -64,39 +57,24 @@ def getFramesGenerator():
             midpoint = recangle.shape[1]//2
             for i in range(6):
                 pixelL = np.argmin(recangle[i*40+20][0:midpoint])
-                widthLine = np.argmax(recangle[i*40+20][pixelL:])
-                pixelL = pixelL + int(widthLine/2)
                 recangle = cv.circle(recangle, (pixelL, i*40+20), 5, (110), -1)
-
                 pixelR = np.argmin(recangle[i*40+20][midpoint:])+midpoint
-                widthLine = np.argmax(recangle[i*40+20][pixelR:])
-                pixelR = pixelR + int(widthLine/2)
                 recangle = cv.circle(recangle, (pixelR, i*40+20), 5, (110), -1)
-                
                 error = (midpoint-pixelL)+(midpoint-pixelR)
                 print (str(pixelL), '\t', str(pixelR), '\t', str(error))
-            _, buf = cv.imencode('.jpg', recangle)
-            buffer[1] = 1
-            buffer[2] = clamp(int(speed+(error*0.5)), 0, 255)
-            buffer[3] = 1
-            buffer[4] = clamp(int(speed-(error*0.5)), 0, 255)
-            robot.write(buffer)
             print ('='*20)
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + buf.tobytes() + b'\r\n')
+            buffer[1] = 1
+            buffer[2] = clamp(int(speed+(error)), 0, 255)
+            buffer[3] = 1
+            buffer[4] = clamp(int(speed-(error)), 0, 255)
+            robot.write(buffer)
+                
+            
 
-@app.route('/')
-def index():
-    """ Выводим видеопоток """
-    return Response(getFramesGenerator(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 if __name__ == '__main__':
-    # пакет, посылаемый на робота
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--port', type=int, default=5000, help="Running port")
-    parser.add_argument("-i", "--ip", type=str, default='172.16.0.49', help="Ip address")
-    args = parser.parse_args()
-    
-    app.run(debug=False, host=args.ip, port=args.port)   # запускаем flask приложение
+    getFramesGenerator()
+
 
 robot.close()
